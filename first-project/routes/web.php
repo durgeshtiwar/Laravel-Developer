0df9a534ccc0f7e74ec358751d0e4088ad2142a2
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-12-27 15:38:05 +0530

    Routing Laravel

---
+++ first-project/routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/about', function () {
+    return "<h1>About Page</h1>";
+});
+Route::get('/user/{id?}', function($id = 4) {
+    return "<h1>User_id is = ".$id."</h1>";
+});
+// Route::get('/user/{developer}/{id}', function($developer,$id) {
+//     return "<h1>User_id is = ".$id."</h1><h1>Developer is = ".$developer."</h1>";
+// })->where(['developer'=>'[A-Za-z]+','id'=>'[0-9]+']);
+// helper Function
+Route::get('/user/{developer}/{id}', function($developer,$id) {
+    return "<h1>User_id is = ".$id."</h1><h1>Developer is = ".$developer."</h1>";
+})->whereAlpha('developer')->whereNumber('id');
+Route::get('yaha', function (){
+    return "<h1>Yaha Wala Page</h1>";
+});
+Route::get('waha', function (){
+    return "<h1>Waha Wala Page</h1>";
+});
+Route::redirect('yaha', 'waha');
+
+
+//Route Prefix
+Route::prefix('admin')->group(function(){
+    Route::get('order', function () {
+        return "<h1>Admin Order Page</h1>";
+    });
+    Route::get('report', function () {
+        return "<h1>Admin report Page</h1>";
+    });
+    Route::get('shipping', function () {
+        return "<h1>Admin shipping Page</h1>";
+    });    
+});
---
+++ second-prject/resources/views/welcome.blade.php
@@ -0,0 +1,1 @@
+<h1>Welcome in Second Page</h1>
