af95640de7430c1aec828b132efc42f9e14b7d0d
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-12-28 17:32:19 +0530

    Controllers

---
+++ first-project/resources/views/About.blade.php
@@ -0,0 +1,15 @@
+<div>
+    <!-- Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less. - Marie Curie -->
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>About Page</h1>
+        <h1>My Name is {{$name}}</h1>
+    </body>
+    </html>
+</div>
