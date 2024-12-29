8e173f0a48cc322446b9607974d03bf3319db8f6
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-12-29 17:14:02 +0530

    Eloquent Model

--- first-project/app/Http/Controllers/DBconnectionController.php
+++ first-project/app/Http/Controllers/DBconnectionController.php
@@ -9,7 +9,10 @@ class DBconnectionController extends Controller
 {
     function DBconnection()
     {
-        $getStudents = DB::select('select * from students');
+        //$getStudents = DB::select('select * from students');
+        $data = new \App\Models\Student;
+        $data->dummy();
+        $getStudents = \App\Models\Student::all();
         return view('Students',['getStudents'=>$getStudents]);
     }
 }
